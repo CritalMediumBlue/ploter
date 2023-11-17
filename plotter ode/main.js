5dd160b7c8b93ea927e8dc374e0ebaa8897e5ce5
CritalMediumBlue <santandergualdron@gmail.com>
2023-11-17 09:40:22 +0100

    realitic gene regulatory network

--- plotter ode/main.js
+++ plotter ode/main.js
@@ -1,58 +1,74 @@
-var pointCount = 1000;
+var pointCount = 5000;
 var i, r;
 
-let prod_R3 = 0.02; // molecule sec-1 Assuming P3 is already defined
-let prod_R1 = 0.15; // Assuming P1 is already defined
-let deg_R = 0.2; // h-1Assuming DR is already defined
-let deg_I = 0.2; // h-1Assuming DI is already defined
-let deg_L = 0.6; // h-1 Assuming DL is already defined
-let for_com_RI = 0.32; // #-1 h-1 or 2.73x10^4 M^-1 s^-1 Assuming KonRI is already defined
-let for_com_RL = 0.32; // #-1 h-1 or 4.85 x10^3 M^-1 s^-1Assuming KonRL is already defined
-let prod_L = 0.2; // Molecules second-1 Assuming PL is already defined
-let n_R = 1; // Assuming nR is already defined
-let n_A = 1; // Assuming nA is already defined
-let K_R = 3; // Assuming KR is already defined
-let K_A = 3; // Assuming KA is already defined
+let prod_R3 = 0.84; // proteins/(transcript*minute)
+let prod_R1 = 0.9; // proteins/(transcript*minute)
+let prod_L = 0.8; // proteins/(transcript*minute)
+let prod_MP1 = 9.0; // transcript/minute
+let prod_MP3 = 10.8; // transcript/minute 
+let prod_ML = 9.0; // transcript/minute
+let deg_R = 3.33e-3;  // 1/minute  ## fixed
+let deg_I = 3.33e-3; // 1/minute ## fixed
+let deg_L = 0.01; // 1/minute ## fixed
+let deg_MP = 0.3; // 1/minute
+
+let for_com_RI =  8.17e-4; // 1/(minute*protein)
+let for_com_RL = 0.0; 
+
+let n_R = 2; 
+let n_A = 0; 
+let K_R = 100; 
+let K_A = 0; 
 let A = 6;
 
 
-var initialValues = [];
-var grid_size = 4;
-var range = [0, 6]; // Adjust as needed
-
-for (let x = 0; x < grid_size; x++) {
-    for (let y = 0; y < grid_size; y++) {
-        for (let z = 0; z < grid_size; z++) {
-            initialValues.push([
-                range[0] + (range[1] - range[0]) * x / (grid_size - 1),
-                range[0] + (range[1] - range[0]) * y / (grid_size - 1),
-                range[0] + (range[1] - range[0]) * z / (grid_size - 1)
-            ]);
-        }
-    }
-}
+var initialValues = [280.0, 40.0, 10.0, 0.0, 0.0, 0.0];
+var grid_size = 2;
+var range = [0, 10]; // Adjust as needed
 var data = [];
 
-for (let j = 0; j < initialValues.length; j++) {
-    var R = [initialValues[j][0]];
-    var I = [initialValues[j][1]];
-    var L = [initialValues[j][2]];
+
+
+    var R = [initialValues[0]];
+    var I = [initialValues[1]];
+    var L = [initialValues[2]];
+    var MP1 = [initialValues[3]];
+    var MP3 = [initialValues[4]];
+    var ML = [initialValues[5]];
     var c = [];
 
     for(i = 0; i < pointCount; i++) {
+
+        let activation_P1 =0.5;// 1 / (1 + Math.pow(R[i] / K_R, n_R)) * Math.pow(A[i] / K_A, n_A) / (1 + Math.pow(A[i] / K_A, n_A));
+       
+        let activation_L = 1 / (1 + Math.pow(R[i] / K_R, n_R));
+        // dMP3/dt equation
+
+        let dMP3 = prod_MP3 - deg_MP*MP3[i];
+        MP3.push(MP3[i] + dMP3/60);
+
+        // dM1/dt equation
+        let dMP1 = prod_MP1*activation_P1 - deg_MP*MP1[i];
+        MP1.push(MP1[i] + dMP1/60);
+
+        // dML/dt equation
+        let dML = prod_ML*activation_L - deg_MP*ML[i];
+        ML.push(ML[i] + dML/60);
+
         // dR/dt equation
-        let activation_R = prod_R1 / (1 + Math.pow(R[i] / K_R, n_R)) * Math.pow(A[i] / K_A, n_A) / (1 + Math.pow(A[i] / K_A, n_A));
-        let dR = prod_R3 + activation_R - deg_R * R[i] - for_com_RI * R[i] * I[i] - for_com_RL * R[i] * L[i]; /* + dis_com_L * ComL[i] + dis_com_I * ComI[i]; */
-        R.push(R[i] + dR);
+         let dR = (MP1[i]+MP3[i])*prod_R3 - deg_R * R[i]- for_com_RI * R[i] * I[i];// - for_com_RL * R[i] * L[i]; /* + dis_com_L * ComL[i] + dis_com_I * ComI[i]; */
+        R.push(R[i] + dR/60);
+
         
         // dI/dt equation
-        let dI = activation_R - deg_I * I[i] - for_com_RI * R[i] * I[i]; /* + dis_com_I * ComI[i]; */
-        I.push(I[i] + dI);
+        let dI = (MP1[i])*prod_R1 - deg_I * I[i]- for_com_RI* R[i] * I[i];           
+        // ; /* + dis_com_I * ComI[i]; */
+        I.push(I[i] + dI/60);
         
         // dL/dt equation
-        let activation_L = prod_L / (1 + Math.pow(R[i] / K_R, n_R));
-        let dL = activation_L - deg_L * L[i] - for_com_RL * R[i] * L[i]; /* + dis_com_L * ComL[i]; */
-        L.push(L[i] + dL);
+        
+        let dL = (ML[i])*prod_L - deg_L * L[i];
+        L.push(L[i] + dL/60);
 
         c.push(i)
     }
@@ -68,17 +84,17 @@ for (let j = 0; j < initialValues.length; j++) {
             color: c,
             colorscale: "Viridis",
             cmin: 0,
-            cmax: 1000
+            cmax: pointCount
         },
         marker: {
             size: 1,
             color: c,
             colorscale: "Viridis",
             cmin: 0,
-            cmax: 1000
+            cmax: pointCount
         }
     });
-}
+
 
 Plotly.newPlot('myDiv', data, {
     scene: {
