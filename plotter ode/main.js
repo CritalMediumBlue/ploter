05f07546597db613b523f5ed28877fb8ce569d42
CritalMediumBlue <santandergualdron@gmail.com>
2023-11-18 09:02:07 +0100

    Final

--- plotter ode/main.js
+++ plotter ode/main.js
@@ -1,4 +1,4 @@
-var pointCount = 50000; //5000 = 2.7 hours = 166.7 minutes
+var pointCount = 10000; //  10000 = 8.32 hours
 var i, r;
 
 /* - Weakly transcribed genes: 0.1 to 1 transcripts per minute (6 to 60 transcripts per hour)
@@ -12,10 +12,14 @@ var i, r;
 
 - **mRNA Degradation rate:** on the order of 0.5 to 3 min^-1 (30 to 180 h^-1)
 
+
+- **High-affinity binding:** K_d values are in the nanomolar range (nM, 10^-9 M). This indicates very tight binding, and such repressors or activators will be strongly associated with their DNA targets even at low concentrations.
+- **Moderate-affinity binding:** K_d values are in the low micromolar range (µM, 10^-6 M). Proteins with moderate binding affinities may require higher concentrations to efficiently bind to their DNA targets.
+- **Low-affinity binding:** K_d values are in the high micromolar to millimolar range (mM, 10^-3 M). Such interactions are weaker, and proteins may dissociate more readily from the DNA, potentially regulating target genes in a more transient fashion
  */
 
 let prod_R3 = 3.0; // (3.33 minutes^-1 acc to Zhuo Chen) 1 (0.84 minutes^-1 acc to Shristopher A. Voigt) (0.84 acc to Jennifer S. Hallinan)
-let prod_R1 = 3.0; //(3.33 minutes^-1 acc to Zhuo Chen) 1 (48 minutes^-1 acc to Shristopher A. Voigt)
+let prod_R1 = 6.0; //(3.33 minutes^-1 acc to Zhuo Chen) 1 (48 minutes^-1 acc to Shristopher A. Voigt)
 let prod_L = 3.0; // (3.33 minutes^-1 acc to Zhuo Chen) 1 (6 minutes^-1 acc to Jennifer S. Hallinan)
 
 let prod_MP1 = 2.0 ; // (1.416 transcript/minute acc to Zhuo Chen)1  (9 transcript/minute acc to Shristopher A. Voigt) (1.2 trancript/minute acc to Jennifer S. Hallinan)
@@ -31,13 +35,13 @@ let deg_MP = 0.5; //  (0.138 1/minute acc to Zhuo Chen)1
 let for_com_RI =  9e-3; //  (5.33e-3 molecules^-1*min^-1 acc to Zhuo Chen)1 (9.06e-3 molecules^-1*min^-1 acc to Joseph A. Newman)
 let for_com_RL = 2e-3; //  (5.33e-3 molecules^-1*min^-1 acc to Zhuo Chen)1 (1.61e-3 molecules^-1*min^-1 acc to Joseph A. Newman)
 
-let n_R = 2; 
-let n_A = 2; 
-let K_R = 400; 
-let K_A = 100; 
+let n_R = 4; 
+let n_A = 8; 
+let K_R = 1700; // (476 - 964 molecules acc Joseph A. Newman)
+let K_A = 45; 
 
 let init_I=0.005; // at the beginning, there is no SinI. AbrB is inhibiting the transcription of SinI and there's no Spo0A-P to activate it.
-let init_L=21.8; // at the beginning, there is no SlrR. SinR is inhibiting the transcription of SlrR.
+let init_L=22; // at the beginning, there is no SlrR. SinR is inhibiting the transcription of SlrR.
 let init_m=0.05;
 
 var initialValues = [];
@@ -58,6 +62,7 @@ for (var j = 0; j <  initialValues.length   ; j++) {
     var ML = [initialValues[j][5]];
     var P = [initialValues[j][6]];
     var c = [];
+    var time_step = 20;
 
     for(i = 0; i < pointCount; i++) {
 
@@ -69,40 +74,58 @@ for (var j = 0; j <  initialValues.length   ; j++) {
         // dMP3/dt equation
 
         let dMP3 = prod_MP3 - deg_MP*MP3[i];
-        MP3.push(MP3[i] + dMP3/30);
+        MP3.push(MP3[i] + dMP3/time_step);
 
         // dM1/dt equation
         let dMP1 = prod_MP1*activation_P1 - deg_MP*MP1[i];
-        MP1.push(MP1[i] + dMP1/30);
+        MP1.push(MP1[i] + dMP1/time_step);
 
         // dML/dt equation
         let dML = prod_ML*activation_L - deg_MP*ML[i];
-        ML.push(ML[i] + dML/30);
+        ML.push(ML[i] + dML/time_step);
 
         // dR/dt equation
          let dR = ((MP1[i])/10 + MP3[i])*prod_R3 - deg_R * R[i]- for_com_RI * R[i] * I[i];// - for_com_RL * R[i] * L[i]; /* + dis_com_L * ComL[i] + dis_com_I * ComI[i]; */
-        R.push(R[i] + dR/30);
+        R.push(R[i] + dR/time_step);
 
         
         // dI/dt equation
         let dI = (MP1[i])*prod_R1 - deg_I * I[i]- for_com_RI* R[i] * I[i];           
         // ; /* + dis_com_I * ComI[i]; */
-        I.push(I[i] + dI/30);
+        I.push(I[i] + dI/time_step);
         
         // dL/dt equation
         
         let dL = (ML[i])*prod_L - deg_L * L[i];
-        L.push(L[i] + dL/30);
+        L.push(L[i] + dL/time_step);
 
         c.push(i)
     }
 
+// Create new arrays for concentrations
+var R_conc = [];
+var I_conc = [];
+var L_conc = [];
+var MP3_conc = [];
+var MP1_conc = [];
+var ML_conc = [];
+
+// After all calculations
+for(i = 0; i < pointCount; i++) {
+    R_conc.push(R[i] / 2);
+    I_conc.push(I[i] / 2);
+    L_conc.push(L[i] / 2);
+    MP3_conc.push(MP3[i] / 2);
+    MP1_conc.push(MP1[i] / 2);
+    ML_conc.push(ML[i] / 2);
+}
+
     data1.push({
         type: 'scatter3d',
         mode: 'lines+markers',
-        x: R, //SinR
-        y: I, //SinI
-        z: L, //SlrR
+        x: R_conc, //SinR
+        y: I_conc, //SinI
+        z: L_conc, //SlrR
         line: {
             width: 5,
             color: c,
@@ -122,9 +145,9 @@ for (var j = 0; j <  initialValues.length   ; j++) {
 data2.push({
     type: 'scatter3d',
     mode: 'lines+markers',
-    x: MP3, //SinR
-    y: MP1, //SinI
-    z: ML, //SlrR
+    x: MP3_conc, //SinR
+    y: MP1_conc, //SinI
+    z: ML_conc, //SlrR
     line: {
         width: 5,
         color: c,
@@ -145,19 +168,19 @@ data2.push({
 Plotly.newPlot('myDiv2', data2, {
     scene: {
         xaxis: {
-            title: 'SinR mRNA [molecules]',
+            title: 'SinR mRNA [nM]',
             titlefont: {
                 color: 'red'
             }
         },
         yaxis: {
-            title: 'SinI mRNA [molecules]',
+            title: 'SinI mRNA [nM]',
             titlefont: {
                 color: 'blue'
             }
         },
         zaxis: {
-            title: 'SlrR mRNA [molecules]',
+            title: 'SlrR mRNA [nM]',
             titlefont: {
                 color: 'rgb(255, 200, 0)'
             }
@@ -169,19 +192,19 @@ Plotly.newPlot('myDiv2', data2, {
 Plotly.newPlot('myDiv', data1, {
     scene: {
         xaxis: {
-            title: 'SinR [molecules]',
+            title: 'SinR [nM]',
             titlefont: {
                 color: 'red'
             }
         },
         yaxis: {
-            title: 'SinI [molecules]',
+            title: 'SinI [nM]',
             titlefont: {
                 color: 'blue'
             }
         },
         zaxis: {
-            title: 'SlrR [molecules]',
+            title: 'SlrR [nM]',
             titlefont: {
                 color: 'rgb(255, 200, 0)'
             }
