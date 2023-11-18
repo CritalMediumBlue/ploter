a8ad4babfeab5fd0996af0fa309a0acee3760eb1
CritalMediumBlue <santandergualdron@gmail.com>
2023-11-18 10:40:41 +0100

    Final for sure

--- plotter ode/main.js
+++ plotter ode/main.js
@@ -1,4 +1,4 @@
-var pointCount = 10000; //  10000 = 8.32 hours
+var pointCount = 5000; //  5000 = 4.16 hours
 var i, r;
 
 /* - Weakly transcribed genes: 0.1 to 1 transcripts per minute (6 to 60 transcripts per hour)
@@ -26,8 +26,8 @@ let prod_MP1 = 2.0 ; // (1.416 transcript/minute acc to Zhuo Chen)1  (9 transcri
 let prod_MP3 = 2.0; // (1.667 transcript/minute acc to Zhuo Chen)1 (16.8 transcript/minute acc to Shristopher A. Voigt)
 let prod_ML = 2.0; //   (2.083 transcript/minute acc to Zhuo Chen)1 (12 transcript/minute acc to Jennifer S. Hallinan)
 
-let deg_R = 5e-3;  //  (3.33e-3 1/minute acc to Zhuo Chen) 1 (0.12 1/minute acc to Shristopher A. Voigt)
-let deg_I = 5e-3; //  (3.33e-3 1/minute acc to Zhuo Chen) 1 (1.2 1/minute acc to Shristopher A. Voigt)
+let deg_R = 8e-3;  //  (3.33e-3 1/minute acc to Zhuo Chen) 1 (0.12 1/minute acc to Shristopher A. Voigt)
+let deg_I = 8e-3; //  (3.33e-3 1/minute acc to Zhuo Chen) 1 (1.2 1/minute acc to Shristopher A. Voigt)
 let deg_L = 0.015; // (0.01 1/minute acc to Zhuo Chen) 1
 
 let deg_MP = 0.5; //  (0.138 1/minute acc to Zhuo Chen)1 
@@ -37,18 +37,18 @@ let for_com_RL = 2e-3; //  (5.33e-3 molecules^-1*min^-1 acc to Zhuo Chen)1 (1.61
 
 let n_R = 4; 
 let n_A = 8; 
-let K_R = 1700; // (476 - 964 molecules acc Joseph A. Newman)
-let K_A = 900; 
+let K_R = 800; // (476 - 964 molecules acc Joseph A. Newman)
+let K_A = 500; 
 
 let init_I=0.005; // at the beginning, there is no SinI. AbrB is inhibiting the transcription of SinI and there's no Spo0A-P to activate it.
-let init_L=22; // at the beginning, there is no SlrR. SinR is inhibiting the transcription of SlrR.
+let init_L=300; // at the beginning, there is no SlrR. SinR is inhibiting the transcription of SlrR.
 let init_m=0.05;
 
 var initialValues = [];
-for (var i = 0; i <= 10; i++) {
-    initialValues.push([2380 , init_I, init_L, init_m, 4.0, 0.11, i*200]);
+for (var i = 30; i <= 70; i++) {
+    initialValues.push([200 , init_I, init_L, init_m, 4.0, 0.11, i*10]);
 }
-//  [R    , I      , L    , mI  , mR  , mL , A0 ];
+//                     [R    , I      , L    , mI   , mR  , mL ,  A0 ];
 var data1 = [];
 var data2 = [];
 
@@ -85,7 +85,7 @@ for (var j = 0; j <  initialValues.length   ; j++) {
         ML.push(ML[i] + dML/time_step);
 
         // dR/dt equation
-         let dR = ((MP1[i])/10 + MP3[i])*prod_R3 - deg_R * R[i]- for_com_RI * R[i] * I[i];// - for_com_RL * R[i] * L[i]; /* + dis_com_L * ComL[i] + dis_com_I * ComI[i]; */
+         let dR = ((MP1[i])/3 + MP3[i])*prod_R3 - deg_R * R[i]- for_com_RI * R[i] * I[i];// - for_com_RL * R[i] * L[i]; /* + dis_com_L * ComL[i] + dis_com_I * ComI[i]; */
         R.push(R[i] + dR/time_step);
 
         
@@ -106,18 +106,12 @@ for (var j = 0; j <  initialValues.length   ; j++) {
 var R_conc = [];
 var I_conc = [];
 var L_conc = [];
-var MP3_conc = [];
-var MP1_conc = [];
-var ML_conc = [];
 
 // After all calculations
 for(i = 0; i < pointCount; i++) {
     R_conc.push(R[i] / 2);
     I_conc.push(I[i] / 2);
     L_conc.push(L[i] / 2);
-    MP3_conc.push(MP3[i] / 2);
-    MP1_conc.push(MP1[i] / 2);
-    ML_conc.push(ML[i] / 2);
 }
 
     data1.push({
@@ -145,9 +139,9 @@ for(i = 0; i < pointCount; i++) {
 data2.push({
     type: 'scatter3d',
     mode: 'lines+markers',
-    x: MP3_conc, //SinR
-    y: MP1_conc, //SinI
-    z: ML_conc, //SlrR
+    x: MP3, //SinR
+    y: MP1, //SinI
+    z: ML, //SlrR
     line: {
         width: 5,
         color: c,
@@ -168,19 +162,19 @@ data2.push({
 Plotly.newPlot('myDiv2', data2, {
     scene: {
         xaxis: {
-            title: 'SinR mRNA [nM]',
+            title: 'SinR mRNA [Molecules]',
             titlefont: {
                 color: 'red'
             }
         },
         yaxis: {
-            title: 'SinI mRNA [nM]',
+            title: 'SinI mRNA [Molecules]',
             titlefont: {
                 color: 'blue'
             }
         },
         zaxis: {
-            title: 'SlrR mRNA [nM]',
+            title: 'SlrR mRNA [Molecules]',
             titlefont: {
                 color: 'rgb(255, 200, 0)'
             }
